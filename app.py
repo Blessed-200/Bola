b8042dce5ac5208c977e7465b23cbf21a189ddf2
Blessed-200 <ameloelsner@gmail.com>
2025-11-25 14:50:50 -0500

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ st.set_page_config(
 TARGET_DRAWS = [('GA', 'Midday'), ('FL', 'Midday'), ('NY', 'Midday'), 
                 ('GA', 'Evening'), ('FL', 'Evening'), ('NY', 'Evening'), 
                 ('GA', 'Night')]
-SEQUENCE_LENGTH = 14
+SEQUENCE_LENGTH = 5
 categorical_features = ['Estado', 'Horario']
 
 # ----------------------------------------------------------------------------------
